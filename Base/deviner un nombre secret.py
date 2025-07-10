0ce06137a3c7d4b0eb14e0980212b9022641702b
djamal-13 <djamal.13pro@gmail.com>
2025-07-10 18:57:37 +0200

    Create deviner un nombre secret.py

---
+++ Base/deviner un nombre secret.py
@@ -0,0 +1,7 @@
+#Deviner un Nombre 42
+
+nombre_secret = int(input("Saissez un nombre : "))
+if nombre_secret == 42:
+    print("Le nomre est correct ")
+else :
+    print("le nombre est incorect ")
