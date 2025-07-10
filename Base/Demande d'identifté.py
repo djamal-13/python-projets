eb35bc3c9485abe395fa2818ef7ed95d15792980
djamal-13 <djamal.13pro@gmail.com>
2025-07-10 18:57:47 +0200

    Delete Demande d'identifté.py

--- Base/Demande d'identifté.py
+++
@@ -1,5 +0,0 @@
-# Demander nom et prénom
-Prenom = input("Saisissez votre Prénom ")
-Nom_de_famille = input("Saisissez votre Nom de famille ")
-print ("Voici votre prénom" ,Prenom, )
-print ("Voici votre prénom" ,Nom_de_famille, )
