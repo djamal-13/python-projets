ad8ab75840b101aa3850805491b5953d3a2903c2
djamal-13 <djamal.13pro@gmail.com>
2025-07-10 18:58:42 +0200

    Update Demande d'identité.py

--- Base/Demande d'identité.py
+++ Base/Demande d'identité.py
@@ -2,4 +2,4 @@
 Prenom = input("Saisissez votre Prénom ")
 Nom_de_famille = input("Saisissez votre Nom de famille ")
 print ("Voici votre prénom" ,Prenom, )
-print ("Voici votre prénom" ,Nom_de_famille, )
+print ("Voici votre Nom" ,Nom_de_famille, )
