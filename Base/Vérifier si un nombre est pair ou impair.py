fca88eaad226917ca7e4a169e72d5e5d93e8dcff
djamal-13 <djamal.13pro@gmail.com>
2025-07-10 18:57:33 +0200

    Create Vérifier si un nombre est pair ou impair.py

---
+++ Base/Vérifier si un nombre est pair ou impair.py
@@ -0,0 +1,15 @@
+#Vérifier si un nombre est pair ou impair
+
+#demander a l'utilisateur de saisir un nombree
+
+nbr = float(input("Veuillez saisir un nombre "))
+
+#verifier si il est pair ou impaire
+
+if nbr % 2 == 0:
+    print("le nombre est pair")
+else:
+    print("le nombre est impaire ")
+
+
+#affichez le resultat
